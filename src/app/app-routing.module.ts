e688521d2710ab377a070d36609e837409c9e48f
Dominique216 <raquel_0216@yahoo.com>
2024-04-12 16:01:54 -0500

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BrowseComponent } from './browse/browse.component';
+import { GetStartedComponent } from './get-started/get-started.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LandingPageComponent}, 
+  {path: 'browse', component: BrowseComponent}, 
+  {path: 'getstarted', component: GetStartedComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { NavComponent } from './nav/nav.component';
+import { BrowseComponent } from './browse/browse.component';
+import { GetStartedComponent } from './get-started/get-started.component';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-    NavComponent
+    NavComponent,
+    BrowseComponent,
+    GetStartedComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    RouterOutlet, 
+    RouterLink, 
+    RouterLinkActive
   ],
   providers: [],
   bootstrap: [AppComponent]
