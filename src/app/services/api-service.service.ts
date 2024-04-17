88b0e5d447e18a8fc93e53887bea0a040b91b085
DominiqueNix <dominiquenix@google.com>
2024-04-17 15:49:20 -0500

    set up access token endpoint and set up new albums to get real data

--- src/app/services/api-service.service.ts
+++ src/app/services/api-service.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http'
-import {BehaviorSubject, Observable, of} from 'rxjs'
+import {BehaviorSubject, Observable, catchError, map, of, switchMap, tap, throwError} from 'rxjs'
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -10,15 +10,26 @@ export class ApiService{
   
   constructor(private http: HttpClient) { }
 
-  getAccessToken(): Observable<{}>{
+  getAccessToken(): Observable<{}> {
     const headers = new HttpHeaders({
-      'Content_type' : 'application/x-www-form-urlencoded'
+      'Content-Type':'application/x-www-form-urlencoded'
     })
-    return this.http.post('https://accounts.spotify.com/api/token', `grant_type=client_credentials&client_id=${environment.clientId}&client_secret=${environment.clientSecret}` , {headers: headers})
+    const body = new URLSearchParams({
+      grant_type: 'client_credentials',
+      client_id: environment.clientId, 
+      client_secret: environment.clientSecret
+    })
+    return  this.http.post('https://accounts.spotify.com/api/token', body.toString(), {headers})
   }
 
-  // getPopularAlbums(): Observable<{}>{
-  //   return this.http.get<{}>();
-  // }
+  getNewAlbums(): Observable<{}>{ 
+    return this.getAccessToken().pipe(
+      switchMap((accessToken:any) => {
+         const headers = {
+          'Authorization':`${accessToken.token_type} ${accessToken.access_token}`
+        };
+        return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=5', {headers})
+      })
+    )
+  }
 }
- 
