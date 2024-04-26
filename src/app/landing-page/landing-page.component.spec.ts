3c80a0726af48c276d07bf52d7a822e6a3abab68
Dominique216 <raquel_0216@yahoo.com>
2024-04-26 16:38:21 -0500

    started apiService testing

--- src/app/landing-page/landing-page.component.spec.ts
+++ src/app/landing-page/landing-page.component.spec.ts
@@ -20,4 +20,24 @@ describe('LandingPageComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have two images', () => {
+    const html = fixture.nativeElement as HTMLElement;
+
+    let images = html.querySelectorAll('img')
+    let img1 = html.querySelectorAll('img')[0]
+    let img2 = html.querySelectorAll('img')[1]
+    
+    expect(images.length).toBe(2)
+    expect(img1.getAttribute('src')).toBe('../../assets/Ellipse 1.png')
+    expect(img2.getAttribute('src')).toBe('../../assets/3dicons.png')
+  })
+
+  it('should have a button with the correct link', () => {
+    const html = fixture.nativeElement as HTMLElement;
+    const a = html.querySelector('a')
+    console.log(a?.getAttribute('routerLink'))
+    expect(a?.children[0]).toBeTruthy()
+    expect(a?.getAttribute('routerLink')).toBe('/getstarted')
+  })
 });
--- src/app/services/api-service.service.spec.ts
+++ src/app/services/api-service.service.spec.ts
@@ -1,16 +1,51 @@
-// import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
-// import { ApiService } from './api-service.service';
+import { ApiService } from './api-service.service';
 
-// describe('ApiServiceService', () => {
-//   let service: ApiService;
+describe('ApiServiceService', () => {
+  let service: ApiService;
+  let httpMock: HttpTestingController;
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({});
-//     service = TestBed.inject(ApiService);
-//   });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    service = TestBed.inject(ApiService);
+    httpMock = TestBed.inject(HttpTestingController)
+  });
 
-//   it('should be created', () => {
-//     expect(service).toBeTruthy();
-//   });
-// });
+  afterEach(() => {
+    httpMock.verify();
+  })
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should return an acces token when getAccessToken is called', () => {
+    let accessToken: {} | undefined;
+    let mockedAccessToken = {token_type:'creds',  access_token:'123'}
+    service.getAccessToken().subscribe(res => {
+      accessToken = res;
+    });
+    const req = httpMock.expectOne(
+      'https://accounts.spotify.com/api/token'
+    )
+    req.flush(mockedAccessToken);
+    expect(accessToken).toEqual(mockedAccessToken)
+  })
+  it('should return new albums when getGetNewAlbums is called', () => {
+    let newAlbums: {} | undefined;
+    let mockedData= {items: ['album1', 'album2', 'album3']}
+    service.getNewAlbums().subscribe(res => {
+      newAlbums = res;
+    });
+
+    // const tokenReq = httpMock.expectOne('https://accounts.spotify.com/api/token')
+    // const albumReq = httpMock.expectOne('https://api.spotify.com/v1/browse/new-releases?limit=5')
+    // tokenReq.flush({})
+    // albumReq.flush(mockedData);
+    // expect(newAlbums).toEqual(mockedData)
+  })
+});
